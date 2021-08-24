cd1e84fb164946f90fe98b6f51a38818d0502726
travis weir <tweirtx@gmail.com>
2021-08-23 23:25:04 -0500

    Add tire pressure provisions

--- main.py
+++ main.py
@@ -12,11 +12,23 @@ except FileNotFoundError:
     callsign = "N/A"
 
 while True:
+    # Read parameters from the OBDII system.
     speed = connection.query(obd.commands.SPEED)
     rpm = connection.query(obd.commands.RPM)
     throttle_position = connection.query(obd.commands.RELATIVE_THROTTLE_POS)  # Subject to change with future testing.
     coolant_temp = connection.query(obd.commands.COOLANT_TEMP)
     fuel_level = connection.query(obd.commands.FUEL_LEVEL)
-    output = f"SPEED {speed.mph} MPH RPM {rpm} THROTTLEPOS {throttle_position} TEMP {coolant_temp} FUEL_LEVEL {fuel_level} CALLSIGN {callsign}"
+
+    # Read tire pressures with software defined radio. TODO
+    fl_psi = 30.0  # Testing value
+    fr_psi = 30.0  # Testing value
+    bl_psi = 30.0  # Testing value
+    br_psi = 30.0  # Testing value
+
+    # Construct data string
+    output = f"SPEED {speed.mph} MPH RPM {rpm} THROTTLEPOS {throttle_position} TEMP {coolant_temp} " \
+             f"FUEL_LEVEL {fuel_level} CALLSIGN {callsign} FL_PSI {fl_psi} FR_PSI {fr_psi} BL_PSI {bl_psi}" \
+             f" BR_PSI {br_psi}"
 
     # send to netcat listener on pit laptop
+    # TODO
