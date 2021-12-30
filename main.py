62ea06dfeb23d79f2166cca7c3f3641b88c561c4
Travis Weir <tweirtx@gmail.com>
2021-12-30 13:34:07 -0600

    Update information

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import obd
 # future plans: integrate TPMS with RTL-SDR readings.
 
 # Initialize the OBDII interface.
+# Activate connection on Linux: sudo rfcomm bind rfcomm0 00:1D:A5:22:E3:FD (or other device MAC)
 connection = obd.OBD()
 
 # Load callsign from disk. This is important when using amateur frequencies.
@@ -14,9 +15,10 @@ except FileNotFoundError:
 
 while True:
     # Read parameters from the OBDII system.
-    speed = connection.query(obd.commands.SPEED).value.to("mph")
+    # speed = connection.query(obd.commands.SPEED).value.to("mph") # TODO fix
+    speed = 0
     rpm = connection.query(obd.commands.RPM).value
-    throttle_position = connection.query(obd.commands.RELATIVE_THROTTLE_POS).value  # Subject to change with future testing.
+    throttle_position = connection.query(obd.commands.RELATIVE_THROTTLE_POS).value  # Subject to change with future testing. TODO determine correct
     coolant_temp = connection.query(obd.commands.COOLANT_TEMP).value
     fuel_level = connection.query(obd.commands.FUEL_LEVEL).value
 
@@ -27,9 +29,11 @@ while True:
     br_psi = 30.0  # Testing value
 
     # Construct data string
-    output = f"SPEED {speed.mph} MPH RPM {rpm} THROTTLEPOS {throttle_position} TEMP {coolant_temp} " \
+    output = f"SPEED {speed} MPH RPM {rpm} THROTTLEPOS {throttle_position} TEMP {coolant_temp} " \
              f"FUEL_LEVEL {fuel_level} CALLSIGN {callsign} FL_PSI {fl_psi} FR_PSI {fr_psi} BL_PSI {bl_psi}" \
              f" BR_PSI {br_psi}"
+
+    print(output)
 
     # send to netcat listener on pit laptop
     # TODO
